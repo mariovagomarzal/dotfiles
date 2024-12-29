002bec8a07aa8d1e05836d8114e3a7093fc2e40b
mariovagomarzal <mariovagomarzal@gmail.com>
2024-12-29 19:37:17 +0100

    Fix a typo
    We've used the 'nix-darwin' option `nix.optimise.enable` instead of
    `nix.optimise.automatic` in the `darwin-configurations/host-users.nix`
    file.

--- darwin-configurations/host-users.nix
+++ darwin-configurations/host-users.nix
@@ -21,14 +21,4 @@
       }
     )
     users;
-
-  # List of users managed by 'nix-darwin'.
-  users.knownUsers = let
-    nonAdminUsers =
-      lib.filterAttrs (
-        _: userInfo: !({isAdmin = false;} // userInfo).isAdmin
-      )
-      users;
-  in
-    lib.attrNames nonAdminUsers;
 }
