ab37bc7dd80b6cbe2503562565a9564029fc9d2a
mariovagomarzal <mariovagomarzal@gmail.com>
2025-08-14 10:38:13 +0200

    Simplify skhd fullscreen window functionality

--- home-configurations/mariovagomarzal@Marios-MacBook-Pro/skhd/config/functions/fullscreen-window.sh
+++ home-configurations/mariovagomarzal@Marios-MacBook-Pro/skhd/config/functions/fullscreen-window.sh
@@ -1,26 +1,5 @@
 #!/usr/bin/env bash
 
-# This script toggles a specified window property ($1) related to fullscreen,
-# with the following behavior:
-#   - If the target is "native", it toggles native fullscreen mode for the
-#     focused window. If the space was not previously in fullscreen mode, i.e.,
-#     the window is entering fullscreen, it moves the space next to the space
-#     it belongs to.
-#   - For any other input, it toggles the specified window property (e.g.,
-#     "zoom-fullscreen").
+# This script toggles a specified window property ($1) related to fullscreen.
 
-current_space=$(yabai -m query --spaces --space | jq '.index')
-is_fullscreen=$(yabai -m query --spaces --space | jq '."is-native-fullscreen"')
-
-case $1 in
-  "native")
-    yabai -m window --toggle native-fullscreen
-    if [ $is_fullscreen != "true" ]; then
-      sleep 1
-      ./functions/move-space.sh $((current_space + 1))
-    fi
-    ;;
-  *)
-    yabai -m window --toggle $1
-    ;;
-esac
+yabai -m window --toggle $1
