9bcc4a156b70d3664a3e21a7d825dc21bef502ca
mariovagomarzal <mariovagomarzal@gmail.com>
2025-09-22 22:17:24 +0200

    feat(overlays): implement a builder for pnpm-based Obsidian plugins

--- overlays/obsidian-builders/default.nix
+++ overlays/obsidian-builders/default.nix
@@ -5,4 +5,7 @@
   importBuilder = lib.dotfiles.importBuilder final;
 in {
   buildNpmObsidianPlugin = importBuilder ./npm-plugin.nix {};
+  buildPnpmObsidianPlugin = importBuilder ./pnpm-plugin.nix {
+    pnpm = final.pnpm_9;
+  };
 }
