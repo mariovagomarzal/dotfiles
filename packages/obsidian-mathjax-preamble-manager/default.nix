a24f32e979f38e346118f6247faba05b5d7641cd
mariovagomarzal <mariovagomarzal@gmail.com>
2025-09-25 00:08:45 +0200

    feat(packages): add the MathJax Preamble Manager plugin for Obsidian

---
+++ packages/obsidian-mathjax-preamble-manager/default.nix
@@ -0,0 +1,23 @@
+#############################################
+# Obsidian MathJax Preamble Manager plugin. #
+#############################################
+{
+  buildNpmObsidianPlugin,
+  fetchFromGitHub,
+  lib,
+}:
+buildNpmObsidianPlugin (finalAttrs: {
+  pname = "obsidian-mathjax-preamble-manager";
+  version = "0.2.10";
+
+  src = fetchFromGitHub {
+    owner = "RyotaUshio";
+    repo = "obsidian-mathjax-preamble-manager";
+    rev = finalAttrs.version;
+    sha256 = "sha256-IkwobdbLQogfXs+OkFKFnenWoY6SYSj0vgZ8lNmTBOM=";
+  };
+
+  npmDepsHash = "sha256-5eJ0eeHLuON6g//RTt/tJ1Jj0VdvSUu6DtIUiwRatvE=";
+
+  dist = ".";
+})
