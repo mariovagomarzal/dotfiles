c861aff2cbefa2cd906f23cede708356fb24f505
mariovagomarzal <mariovagomarzal@gmail.com>
2025-09-24 17:15:31 +0200

    feat(packages): add the Style settings plugin for Obsidian
    Instead of using the source for the official repo by @mgmeyers, I'm
    using a fork by me (@mariovagomarzal) that pins the version of
    @simonwep/pickr to an official npm release, istead of GitHub tarball
    URL.
    
    This is done beacause Yarn with Nix seems not to correctly handle
    the dependencies when using a GitHub tarball URL.

---
+++ packages/obsidian-style-settings/default.nix
@@ -0,0 +1,23 @@
+###################################
+# Obsidian Style Settings plugin. #
+###################################
+{
+  buildYarnObsidianPlugin,
+  fetchFromGitHub,
+  runCommand,
+  lib,
+}:
+buildYarnObsidianPlugin (finalAttrs: {
+  pname = "obsidian-style-settings";
+  version = "1.0.9";
+
+  src = fetchFromGitHub {
+    owner = "mariovagomarzal";
+    repo = "obsidian-style-settings";
+    rev = "359c481a24f811c009f9c2812c5188ec968ced17";
+    sha256 = "sha256-6xyp5PE4mhKYP3Lc4vKUG/N3aqBhHGwsCuzfVkq1jwM=";
+  };
+
+  yarnLock = finalAttrs.src + "/yarn.lock";
+  yarnDepsHash = "sha256-tqX09XWI3ZL9bXVdjgsAEuvfCAjnyWj5uSWGFbNApds=";
+})
