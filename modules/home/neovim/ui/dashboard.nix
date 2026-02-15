fab09cf0cae46f02a02a771e78d866436968fd75
mariovagomarzal <mariovagomarzal@gmail.com>
2026-02-15 21:00:13 +0100

    feat(home): configure the neovim dashboard plugin

--- modules/home/neovim/ui/dashboard.nix
+++ modules/home/neovim/ui/dashboard.nix
@@ -11,6 +11,8 @@
           theme = "hyper";
           config = {
             header = [
+              ""
+              ""
               "        ▒█▒              █                   ░▓   ██████         ███████   █   "
               "        ███            ░██                  █░      ██  █       ▒██  ▓      █░ "
               "        ███           ███▓                 ▒█       █ █  █     ░█ █  ▓       █ "
@@ -23,7 +25,58 @@
               "░██▒                   ▒░      █▒   █      ░█                                █ "
               "                               █▒  █        ░█                              █  "
               "                                 █░           █                           ░█   "
+              ""
+              ""
             ];
+            shortcut = [
+              {
+                desc = "󰈞 Find File";
+                group = "@keyword";
+                key = "f";
+                action = "Telescope find_files";
+              }
+              {
+                desc = " Recent Files";
+                group = "@function";
+                key = "r";
+                action = "Telescope oldfiles";
+              }
+              {
+                desc = " Live Grep";
+                group = "@string";
+                key = "g";
+                action = "Telescope live_grep";
+              }
+              {
+                desc = " Dotfiles";
+                group = "@constant";
+                key = "c";
+                action = "Telescope find_files cwd=~/Projects/dotfiles";
+              }
+              {
+                desc = "󰈆 Quit";
+                group = "@comment";
+                key = "q";
+                action = "qa";
+              }
+            ];
+            mru = {
+              limit = 10;
+            };
+            project = {
+              enable = true;
+              limit = 5;
+              action = "Telescope find_files cwd=";
+            };
+            packages.enable = false;
+            footer.__raw = ''
+              function()
+                local v = vim.version()
+                local version = string.format("v%d.%d.%d", v.major, v.minor, v.patch)
+                local date = os.date("%A, %B %d, %Y")
+                return { "", "Neovim " .. version .. "  •  " .. date }
+              end
+            '';
           };
         };
       };
