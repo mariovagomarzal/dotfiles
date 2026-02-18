cc25eb4083b5f1c2c6f1e1d480be4e00f7e9571d
mariovagomarzal <mariovagomarzal@gmail.com>
2026-02-19 00:44:50 +0100

    feat(home): enhance terminal management in Neovim

--- modules/home/neovim/editor/toggleterm.nix
+++ modules/home/neovim/editor/toggleterm.nix
@@ -1,7 +1,26 @@
 #######################################
 # Neovim Toggleterm plugin submodule. #
 #######################################
-{...}: {
+{...}: let
+  # Generate terminal switch keymaps for <leader>t<0-9>.
+  terminalSwitchKeymaps =
+    builtins.genList (i: let
+      n = i + 1;
+      key =
+        if n == 10
+        then "0"
+        else toString n;
+    in {
+      mode = "n";
+      key = "<leader>t${key}";
+      action = "<cmd>lua _G._toggleterm_switch(${toString n})<CR>";
+      options = {
+        desc = "Switch to terminal ${toString n}";
+        silent = true;
+      };
+    })
+    10;
+in {
   programs.nixvim = {
     plugins = {
       # Toggleterm.
@@ -11,12 +30,20 @@
         settings = {
           # General settings.
           hide_numbers = true;
-          start_in_insert = true;
+          start_in_insert = false;
           close_on_exit = true;
           auto_scroll = true;
 
           # Size for non-float directions.
-          size = 15;
+          size.__raw = ''
+            function(term)
+              if term.direction == "vertical" then
+                return math.floor(vim.o.columns * 0.4)
+              else
+                return math.floor(vim.o.lines * 0.3)
+              end
+            end
+          '';
 
           # Default direction.
           direction = "float";
@@ -29,56 +56,100 @@
       };
     };
 
-    # Toggleterm keymaps.
-    keymaps = [
-      # Terminal toggles.
-      {
-        mode = "n";
-        key = "<leader>tt";
-        action = "<cmd>ToggleTerm<CR>";
-        options = {
-          desc = "Toggle terminal";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>tf";
-        action = "<cmd>ToggleTerm direction=float<CR>";
-        options = {
-          desc = "Toggle float terminal";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>th";
-        action = "<cmd>ToggleTerm direction=horizontal<CR>";
-        options = {
-          desc = "Toggle horizontal terminal";
-          silent = true;
-        };
-      }
-      {
-        mode = "n";
-        key = "<leader>tv";
-        action = "<cmd>ToggleTerm direction=vertical<CR>";
-        options = {
-          desc = "Toggle vertical terminal";
-          silent = true;
-        };
-      }
+    # Lua helpers for terminal session management.
+    extraConfigLua = ''
+      _G._toggleterm_last = 1
 
-      # Terminal mode escape.
-      {
-        mode = "t";
-        key = "<esc><esc>";
-        action = "<C-\\><C-n>";
-        options = {
-          desc = "Exit terminal mode";
-          silent = true;
-        };
-      }
-    ];
+      function _G._toggleterm_switch(n)
+        _G._toggleterm_last = n
+        if vim.bo.filetype == "toggleterm" then
+          local current = vim.b.toggle_number
+          if current == n then
+            vim.cmd("ToggleTerm")
+            return
+          end
+          local terms = require("toggleterm.terminal")
+          local term = terms.get(current)
+          local direction = term and term.direction or "float"
+          vim.cmd("ToggleTerm")
+          vim.cmd(n .. "ToggleTerm direction=" .. direction)
+        else
+          vim.cmd(n .. "ToggleTerm direction=float")
+        end
+      end
+
+      function _G._toggleterm_toggle(direction)
+        local n = _G._toggleterm_last
+        if vim.bo.filetype == "toggleterm" then
+          local terms = require("toggleterm.terminal")
+          local term = terms.get(vim.b.toggle_number)
+          local current_dir = term and term.direction or "float"
+          if not direction or direction == current_dir then
+            vim.cmd("ToggleTerm")
+          else
+            vim.cmd("ToggleTerm")
+            vim.cmd(n .. "ToggleTerm direction=" .. direction)
+          end
+        elseif direction then
+          vim.cmd(n .. "ToggleTerm direction=" .. direction)
+        else
+          vim.cmd(n .. "ToggleTerm")
+        end
+      end
+    '';
+
+    # Toggleterm keymaps.
+    keymaps =
+      [
+        # Terminal toggles (uses last used terminal).
+        {
+          mode = "n";
+          key = "<leader>tt";
+          action = "<cmd>lua _G._toggleterm_toggle()<CR>";
+          options = {
+            desc = "Toggle terminal";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>tf";
+          action = "<cmd>lua _G._toggleterm_toggle('float')<CR>";
+          options = {
+            desc = "Toggle float terminal";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>th";
+          action = "<cmd>lua _G._toggleterm_toggle('horizontal')<CR>";
+          options = {
+            desc = "Toggle horizontal terminal";
+            silent = true;
+          };
+        }
+        {
+          mode = "n";
+          key = "<leader>tv";
+          action = "<cmd>lua _G._toggleterm_toggle('vertical')<CR>";
+          options = {
+            desc = "Toggle vertical terminal";
+            silent = true;
+          };
+        }
+
+        # Terminal mode escape.
+        {
+          mode = "t";
+          key = "<esc><esc>";
+          action = "<C-\\><C-n>";
+          options = {
+            desc = "Exit terminal mode";
+            silent = true;
+          };
+        }
+      ]
+      ++ terminalSwitchKeymaps;
   };
 }
--- modules/home/neovim/ui/lualine.nix
+++ modules/home/neovim/ui/lualine.nix
@@ -28,9 +28,48 @@
             lualine_a = ["mode"];
             lualine_b = ["branch" "diff" "diagnostics"];
             lualine_c = [
+              # Toggleterm: terminal number ([N]) with bold mauve accent.
+              {
+                __unkeyed-1.__raw = ''
+                  function()
+                    local nr = vim.b.toggle_number or 0
+                    return "[" .. nr .. "]"
+                  end
+                '';
+                cond.__raw = ''
+                  function()
+                    return vim.bo.filetype == "toggleterm"
+                  end
+                '';
+                color = {gui = "bold";};
+              }
+              # Toggleterm: terminal info (Running <cmd> at <cwd>).
+              {
+                __unkeyed-1.__raw = ''
+                  function()
+                    local bufname = vim.api.nvim_buf_get_name(0)
+                    local cmd = bufname:match("%d+:(.+);#toggleterm")
+                    cmd = cmd and vim.fn.fnamemodify(cmd, ":t") or "terminal"
+                    local cwd = bufname:match("term://(.-)//")
+                    cwd = cwd and vim.fn.fnamemodify(cwd, ":~") or "~"
+                    return "Running " .. cmd .. " at " .. cwd
+                  end
+                '';
+                cond.__raw = ''
+                  function()
+                    return vim.bo.filetype == "toggleterm"
+                  end
+                '';
+              }
+              # Regular buffers: filename with relative path.
               {
                 __unkeyed-1 = "filename";
                 path = 1;
+                cond.__raw = ''
+                  function()
+                    return vim.bo.filetype ~= "toggleterm"
+                  end
+                '';
               }
             ];
             lualine_x = ["encoding" "fileformat" "filetype"];
