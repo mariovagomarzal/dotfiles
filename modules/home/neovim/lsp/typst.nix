3202c6c6dfa099510352b805b9f8b80f435f5ac3
mariovagomarzal <mariovagomarzal@gmail.com>
2026-02-18 01:33:41 +0100

    feat(home): add smart autopairs for dollar signs in Typst

--- modules/home/neovim/lsp/typst.nix
+++ modules/home/neovim/lsp/typst.nix
@@ -15,5 +15,24 @@
       formatters.typststyle.command = lib.getExe pkgs.typstyle;
       formatters_by_ft.typst = ["typststyle"];
     };
+
+    # Custom autopairs behavior for dollar signs.
+    extraConfigLua = ''
+      local npairs = require("nvim-autopairs")
+      local Rule = require("nvim-autopairs.rule")
+      local cond = require("nvim-autopairs.conds")
+
+      npairs.add_rules({
+        -- Auto-pair dollar signs for Typst math delimiters.
+        Rule("$", "$", "typst"),
+
+        -- Space expansion: `$|$` -> `$ | $` (inline to display math).
+        Rule(" ", " ", "typst")
+          :with_pair(function(opts)
+            local pair = opts.line:sub(opts.col - 1, opts.col)
+            return pair == "$$"
+          end),
+      })
+    '';
   };
 }
